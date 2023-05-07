89550b3df26fa7c1923b135357f19b1045088a5e
flfk55 <flfk55@naver.com>
2023-05-07 23:36:38 +0900

    feat : axios를 사용한 REST-API 방식 통신

--- class/pages/04-01-rest-get/index.js
+++ class/pages/04-01-rest-get/index.js
@@ -1,10 +1,35 @@
-// 동기 통신과 비동기 통신
-export default function RestGetPage() {
-   return (
-      <div>
-         {/* 비동기통신 */}
+import axios from "axios";
+import { useState } from "react";
 
+// 동기통신과 비동기통신
+export default function RestGetPage() {
+   const [title, setTitle] = useState('')
+   const onClickAsync = () => {
+      const result = axios.get("https://koreanjson.com/posts/1")
+      console.log(result) // promise{<pending>} - promise 대기 상태
+   }
+
+   // 화살표 함수 async/await
+   const onClickSync = async () => {
+      const result = await axios.get("https://koreanjson.com/posts/1")
+      console.log(result) // {data: {…}, status: 200, statusText: 'OK', headers: AxiosHeaders, config: {…}, …}
+      console.log(result.data)
+      setTitle(result.data.title)
+   }
+
+   // 일반 함수 async/await
+   // async function onClickSync() {
+   //    const result = await axios.get("https://koreanjson.com/posts/1")
+   //    console.log(result)
+   // }
+   return (
+      <>
+         {/* 비동기통신 */}
+         <button onClick={onClickAsync}>REST-API(비동기)요청하기</button>
          {/* 동기통신 */}
-      </div>
+         <button onClick={onClickSync}>REST-API(동기) 요청하기</button>
+
+         제목 : {title}
+      </>
    );
 }
