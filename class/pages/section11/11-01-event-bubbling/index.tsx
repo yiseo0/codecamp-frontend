6373b5d65f621244dd53acf500f43c3ce1b6714b
flfk55 <flfk55@naver.com>
2023-05-19 06:34:31 +0900

    [섹션 33 ~ 34] 이벤트 버블링 및 코드 포멧터와 린터 학습

--- class/pages/section11/11-01-event-bubbling/index.tsx
+++ class/pages/section11/11-01-event-bubbling/index.tsx
@@ -1,5 +1,4 @@
 import { gql, useQuery } from "@apollo/client";
-import styled from "@emotion/styled";
 
 const FETCH_BOARDS = gql`
   query {
@@ -12,36 +11,21 @@ const FETCH_BOARDS = gql`
   }
 `;
 
-const onCLickAlert = (e :  React.MouseEvent<HTMLDivElement>) => {
-    alert(e.currentTarget.id);
+const onCLickAlert = (e: React.MouseEvent<HTMLUListElement>): void => {
+  alert(e.currentTarget.id);
 }
 
-const qqq = () => {
-  alert("클릭 타이틀");
-}
-
-const Row = styled.div`
-  display: flex;
-`;
-const Column = styled.div`
-  width: 25%;
-  border-bottom: 1px solid #666;
-`;
-
-export default function StaticRoutedBoardPage() {
+export default function StaticRoutedBoardPage() : JSX.Element{
   const { data } = useQuery(FETCH_BOARDS);
 
   return (
     <>
-      {data?.fetchBoards.map((el) => (
-        <Row key={el.number} id={el.writer} onClick={onCLickAlert}>
-          <Column>
-            <input type="checkbox" />
-          </Column>
-          <Column>{el.number}</Column>
-          <Column>{el.writer}</Column>
-          <Column>{el.contents}</Column>
-        </Row>
+      {data?.fetchBoards.map((el: any) => (
+        <ul key={el.number} id={el.writer} onClick={onCLickAlert}>
+          <li>{el.number}</li>
+          <li>{el.writer}</li>
+          <li>{el.contents}</li>
+        </ul>
       ))}
     </>
   );
